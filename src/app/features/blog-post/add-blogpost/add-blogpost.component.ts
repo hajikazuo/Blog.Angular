c0409c9e4046d410b8d7d6bbae37bbc884d7c664
hajikazuo <hajikazuo@outlook.com>
2024-09-02 15:06:32 -0300

    feat add blogpost

---
+++ src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-add-blogpost',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './add-blogpost.component.html',
+  styleUrl: './add-blogpost.component.css'
+})
+export class AddBlogpostComponent {
+
+}
