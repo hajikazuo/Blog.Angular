17b5ffa80fd1c05e44176eaaa4693216345cacfd
hajikazuo <hajikazuo@outlook.com>
2024-09-10 16:49:44 -0300

    feat get and creat blogposts

--- src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
+++ src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AddBlogPost } from '../models/add-blog-post.model';
+import { CommonModule } from '@angular/common';
+import { BlogPostService } from '../services/blog-post.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-blogpost',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './add-blogpost.component.html',
   styleUrl: './add-blogpost.component.css'
 })
 export class AddBlogpostComponent {
+  model: AddBlogPost;
+
+  constructor(private blogPostService: BlogPostService,
+    private router: Router) {
+    this.model = {
+      title: '',
+      shortDescription: '',
+      urlHandle: '',
+      content: '',
+      featuredImageUrl: '',
+      author: '',
+      isVisible: true,
+      publishedDate: new Date()
+    }
+  }
+
+  onFormSubmit(): void {
+    this.blogPostService.createBlogPost(this.model)
+      .subscribe({
+        next: (response) => {
+          this.router.navigateByUrl('/admin/blogposts')
+        }
+      });
+  }
 
 }
---
+++ src/app/features/blog-post/services/blog-post.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { AddBlogPost } from '../models/add-blog-post.model';
+import { Observable } from 'rxjs';
+import { BlogPost } from '../models/blog-post.model';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlogPostService {
+
+  constructor(private http: HttpClient) { }
+
+getAllBlogPosts(): Observable<BlogPost[]> {
+  return this.http.get<BlogPost[]>(`${environment.apiBaseUrl}/api/blogposts`)
+}
+
+  createBlogPost(data: AddBlogPost) : Observable<BlogPost> {
+    return this.http.post<BlogPost>(`${environment.apiBaseUrl}/api/blogposts`, data)
+  }
+}
